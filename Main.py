5b3f14d36722972e54cd7a5a1f2cd934e6438d4d
Cyan Skeleton <jhaanubhav8305@gmail.com>
2025-06-12 21:13:26 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,7 @@
 from openai import OpenAI
 import streamlit as st
 
-<<<<<<< Updated upstream
-st.title("StainGPT")
-=======
 st.title("MusicGPT")
->>>>>>> Stashed changes
 
 # Set OpenAI API key from Streamlit secrets
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
